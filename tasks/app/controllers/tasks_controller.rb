104aef01c64e01deb3a73c6dd73d6c67eee3cde2
Artem Rakov <artem@rakov.me>
2021-11-12 13:41:53 +0000

    create new tasks

--- tasks/app/controllers/tasks_controller.rb
+++ tasks/app/controllers/tasks_controller.rb
@@ -2,5 +2,34 @@ class TasksController < ApplicationController
   before_action :authenticate_account!
 
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    employee = Account.employee.sample
+    @task.employee = employee
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def shuffle
+  end
+
+  def complete
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
--- tasks/config/routes.rb
+++ tasks/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resource :session, only: %i[new destroy]
   resources :tasks, only: %i[new create] do
+    post :shuffle, on: :collection
   end
   post 'auth/:provider', to: 'auth#request', as: :auth_request
   get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
